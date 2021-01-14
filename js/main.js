8ea686d512cbefa3915aac0d146448de1188c14a
iTanvir <thisistanvir01@gmail.com>
2021-01-14 18:09:53 +0600

    Design Work and Contact Section

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+
+
 (function ($) {
    "use strict";
    
@@ -8,5 +10,56 @@
    });
    // End Counter
 
+    let index = 0;
+    const totalWorkItems = $('.work-item').length;
+
+    // set lightbox img max height
+    const wHeight = $(window).height();
+    $('.lightbox-img').css('max-height', wHeight + 'px');
+
+    // lightbox
+    $('.work-item-inner').click(function(){
+        index = $(this).parent('.work-item').index();
+        $('.lightbox').addClass('open');
+        lightboxSlideShow();
+    })
+
+    $('.lightbox .prev').click(function(){
+        if(index == 0){
+            index = totalWorkItems-1
+        }else{
+            index--;
+        }
+        lightboxSlideShow();
+    })
+
+    $('.lightbox .next').click(function(){
+        if(index== totalWorkItems-1){
+            index=0
+        }else{
+            index++; 
+        }
+        lightboxSlideShow();
+    })
+
+    // close lightbox
+    $('.lightbox-close').click(function(){
+        $('.lightbox').removeClass('open')
+    })
+
+    // close lightbox when clicked outside of img box
+    $('.lightbox').click(function(event){
+        if($(event.target).hasClass('lightbox')){
+            $(this).removeClass('open');
+        }
+    })
+
+    function lightboxSlideShow(){
+        const imgSrc = $('.work-item').eq(index).find('img').attr('data-large');
+        const category = $('.work-item').eq(index).find('h4').html();
+        $('.lightbox-img').attr('src', imgSrc);
+        $('.lightbox-category').html(category);
+        $('.lightbox-counter').html((index+1) + '/' + totalWorkItems);
+    }
 
 })(jQuery);
